7a76b4d6f78a1d6e54e8dccfa09bec8b09a92fa7
ademnr <ademnr2@gmail.com>
2025-08-16 15:54:21 +0100

    added landing page

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       if (user) {
         router.push("/recognition");
       } else {
-        router.push("/login");
+        router.push("/landing");
       }
     }
   }, [user, loading, router]);
